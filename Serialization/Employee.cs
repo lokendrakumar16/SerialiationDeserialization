d940662c0835033c34f26bbd33c6f4930101fd57
Lokendra Kumar <Lokendra_Kumar@Dellteam.com>
2020-08-22 23:43:00 +0530

    Serialization

---
+++ Serialization/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Serialization
+{
+    [Serializable]
+    class Employee
+    {
+        public int EmployeeId;
+        public string EmployeeName;
+        public Employee(int EmpId, string EmpName)
+        {
+            this.EmployeeId = EmpId;
+            this.EmployeeName = EmpName;
+        }
+    }
+}
---
+++ Serialization/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"C:\Temp\temp.txt";
+
+            /* Serialization
+            Employee emp = new Employee(1, "Lokendra");
+            FileStream stream = new FileStream(filePath, FileMode.OpenOrCreate);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(stream, emp);
+            stream.Close();
+            Console.WriteLine("File saved in " + filePath);
+            Console.ReadLine();
+            */
+
+            // Deserialization
+            FileStream stream = new FileStream(filePath, FileMode.OpenOrCreate);
+            BinaryFormatter formatter = new BinaryFormatter();
+            Employee emp = (Employee)formatter.Deserialize(stream);
+            Console.WriteLine("Employee Id: " + emp.EmployeeId);
+            Console.WriteLine("Employee Name " + emp.EmployeeName);
+            Console.ReadLine();
+
+        }
+    }
+}
